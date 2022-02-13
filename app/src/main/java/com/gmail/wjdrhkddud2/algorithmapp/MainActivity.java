db23ba4429095a641cfc27d0ea3326ec6e9b5266
IamYeong <wjdrhkddud2@gmail.com>
2022-02-14 04:06:04 +0900

    Feat shuffle function and struct layout

--- app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/MainActivity.java
+++ app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/MainActivity.java
@@ -3,12 +3,108 @@ package com.gmail.wjdrhkddud2.algorithmapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView arrayText;
+    private Button bubbleSortButton;
+    private List<Integer> array;
+    private ImageButton shuffleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arrayText = findViewById(R.id.tv_array);
+        shuffleButton = findViewById(R.id.btn_shuffle);
+        bubbleSortButton = findViewById(R.id.btn_bubble_sort);
+
+        shuffleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shuffleArray(array);
+            }
+        });
+
+        bubbleSortButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SortManager sortManager = new SortManager();
+                sortManager.bubbleSort(array);
+
+                updateArrayText();
+
+            }
+        });
+
+
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //List<Integer> array1 = new ArrayList<>();
+        array = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+
+            array.add(i);
+
+        }
+
+        shuffleArray(array);
+
+        updateArrayText();
+
+    }
+
+    private void shuffleArray(List<Integer> array) {
+
+        for (int i = array.size() - 1; i >= 0; i--) {
+
+            int random = (int)(Math.random() * i);
+            int j = array.get(random);
+            int k = array.get(i);
+
+            array.set(random, k);
+            array.set(i, j);
+
+            System.out.println((array.get(i) + ", "));
+
+        }
+
+        updateArrayText();
+
+        //return array;
+    }
+
+    private void updateArrayText() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < array.size(); i++) {
+
+            stringBuilder.append(array.get(i)).append(", ");
+
+        }
+
+        arrayText.setText(stringBuilder.toString());
+
+    }
+
 }
---
+++ app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/SortManager.java
@@ -0,0 +1,16 @@
+package com.gmail.wjdrhkddud2.algorithmapp;
+
+import java.util.List;
+
+public class SortManager {
+
+    public SortManager() {}
+
+    public List<Integer> bubbleSort(List<Integer> data) {
+
+        return data;
+    }
+
+
+
+}
