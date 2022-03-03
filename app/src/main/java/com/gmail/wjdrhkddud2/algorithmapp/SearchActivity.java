5ffbb30c0e6df54dce17d95a9c1ce17265c41b41
IamYeong <wjdrhkddud2@gmail.com>
2022-03-03 17:18:48 +0900

    Added sequential search

--- app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/SearchActivity.java
+++ app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/SearchActivity.java
@@ -3,12 +3,41 @@ package com.gmail.wjdrhkddud2.algorithmapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private HorizontalScrollView horizontalScrollView;
+    private LinearLayout linearHorizontal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        horizontalScrollView = findViewById(R.id.horizontal_search);
+        linearHorizontal = findViewById(R.id.linear_horizontal_search);
+
+        TextView textView = new TextView(SearchActivity.this);
+        textView.setText("AAA");
+        textView.setTextColor(getColor(R.color.purple_700));
+        textView.setTextSize(15f);
+        textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+
     }
+
+    private void updateUI() {
+
+    }
+
+
+
 }
--- app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/SearchManager.java
+++ app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/SearchManager.java
@@ -1,12 +1,41 @@
 package com.gmail.wjdrhkddud2.algorithmapp;
 
+import java.util.List;
+
 public class SearchManager {
 
     public SearchManager() {
 
     }
 
+    /** 2022.03.03 Sequential search
+     * 순차탐색법은 간단하다. 처음부터 확인하면 된다.
+     *
+     * @return
+     */
+    public int sequentialSearch(List<Integer> data, int target) {
 
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i) == target) return i;
+        }
+
+        return -1;
+    }
+
+
+    /** 2022.03.03 Binary search
+     * 이진탐색법,
+     *
+     *
+     * @param data
+     * @param target
+     * @return
+     */
+    public int binarySearch(List<Integer> data, int target) {
+
+
+
+    }
 
 
 
