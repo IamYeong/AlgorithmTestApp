fd9a5e8dcc89d78eda2f04491ef6ea15ce2965cf
IamYeong <wjdrhkddud2@gmail.com>
2022-03-03 17:29:45 +0900

    Added binary search

--- app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/SearchManager.java
+++ app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/SearchManager.java
@@ -25,17 +25,36 @@ public class SearchManager {
 
     /** 2022.03.03 Binary search
      * 이진탐색법,
+     * 업다운 게임과 비슷하다.
+     * 정렬된 데이터를 기준으로 중간값 대비 찾으려는 값이 큰지 작은지 비교해서 쪼개나간다.
      *
-     *
-     * @param data
      * @param target
      * @return
      */
-    public int binarySearch(List<Integer> data, int target) {
+    public int binarySearch(List<Integer> sortedData, int target) {
 
-
+        return realBinarySearch(sortedData, target, 0, sortedData.size() - 1);
 
     }
+
+    private int realBinarySearch(List<Integer> sortedData, int target, int low, int high) {
+
+        //중간 index
+        int middle = (low + high) / 2;
+
+        if (target > middle) {
+
+            return realBinarySearch(sortedData, target, low, middle - 1);
+
+        } else if (target < middle) {
+
+            return realBinarySearch(sortedData, target, middle + 1, high);
+        } else {
+            return middle;
+        }
+
+    }
+
 
 
 
