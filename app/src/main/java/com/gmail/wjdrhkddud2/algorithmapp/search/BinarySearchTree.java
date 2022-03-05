c6ed3135fd7ba6cd94c5283c8044ff8389b23a0f
IamYeong <wjdrhkddud2@gmail.com>
2022-03-05 15:17:09 +0900

    Added binary search tree search func

--- app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/search/BinarySearchTree.java
+++ app/src/main/java/com/gmail/wjdrhkddud2/algorithmapp/search/BinarySearchTree.java
@@ -2,6 +2,40 @@ package com.gmail.wjdrhkddud2.algorithmapp.search;
 
 public class BinarySearchTree {
 
-    
+    private BinaryTreeNode<Integer> root = null;
+
+    public BinaryTreeNode<Integer> searchTree(int target) {
+
+        if (root == null) {
+            return null;
+        } else {
+
+            BinaryTreeNode<Integer> currentNode = root;
+            while (currentNode != null) {
+
+                if (currentNode.getValue() > target) {
+                    currentNode = currentNode.getLeftChild();
+                } else if (currentNode.getValue() < target) {
+                    currentNode = currentNode.getRightChild();
+                } else {
+                    return currentNode;
+                }
+
+            }
+
+        }
+
+        return null;
+    }
+
+    public void insertNode() {
+
+
+
+    }
+
+    public void deleteNode() {
+
+    }
 
 }
